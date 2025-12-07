a7f7ce8232ba18c3ddccc8df325ef2703f39334e
Nico-Sng <nicolassung01@gmail.com>
2025-12-07 02:26:45 +0100

    refactor: Client and Server separate

--- src/hooks/useTheme.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { useSettingsContext } from "@/contexts/SettingsContext";
-
-export function useTheme() {
-    const { theme, setTheme, toggleTheme } = useSettingsContext();
-    return { theme, setTheme, toggle: toggleTheme };
-}
